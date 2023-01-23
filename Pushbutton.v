734946984f636c0d5895f96b33597068bf0cedbc
Aharon44 <102623417+Aharon44@users.noreply.github.com>
2023-01-23 18:33:28 +0200

    Add files via upload

---
+++ Pushbutton.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12.12.2022 17:13:25
+// Design Name: 
+// Module Name: Pushbutton
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Pushbutton(
+    input pb,
+    input clk_in,
+    output led
+    );
+endmodule
